9b4e3e26c8d571ac69e5b6da55c6660b04260104
arthurdmachado <arthur_d_machado@estudante.sesisenai.org.br>
2026-08-18 11:04:32 -0300

    add - cadastrar prato

---
+++ public/cadastrar_prato.php
@@ -0,0 +1,17 @@
+<?php
+
+include "../infra/conexao.php";
+
+$id = $_POST["id"];
+$nome = $_POST["nome"];
+$descricao = $_POST["descricao"];
+$preco = $_POST["preco"];
+$categoria = $_POST["categoria"];
+$id_usuario = $_POST["id_usuario"]
+
+$sql = "INSERT INTO pratos (id,nome,descricao,preco,categoria,id_usuario) VALUES ('$id,$nome,$descricao,$preco,$categoria,$id_usuario')";
+
+mysqli_query($conexao, $sql);
+
+header("Location: ../index.php");
+?>
