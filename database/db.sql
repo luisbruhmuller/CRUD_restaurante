c2a48ff011eb45aab96e0dc82aeaf1ae100fbf62
luisbruhmuller <luis_bruhmuller@estudante.sesisenai.org.br>
2026-08-18 11:03:01 -0300

    fix - script db

--- database/db.sql
+++ database/db.sql
@@ -1,18 +1,17 @@
 CREATE DATABASE crud_restaurante;
 USE crud_restaurante;
 
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(200) NOT NULL,
+    email VARCHAR(200) NOT NULL
+);
 CREATE TABLE pratos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(200) NOT NULL,
     descricao VARCHAR(200)NOT NULL,
     preco DECIMAL NOT NULL,
     categoria VARCHAR(200) NOT NULL,
-    id_usuario INT,
-    FOREIGN KEY id_usuario REFERENCES usuarios(id)
-);
-
-CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(200) NOT NULL,
-    email VARCHAR(200) NOT NULL
+    usuario_id INT,
+   FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
 );
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 include "infra/conexao.php";
-$pratos = mysqli_query($conexao, "SELECT * FROM pratos");
+$prato = mysqli_query($conexao, "SELECT * FROM pratos");
 
 ?>
 
