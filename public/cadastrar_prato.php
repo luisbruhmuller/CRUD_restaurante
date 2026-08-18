28b6627fffd7572eecfa389ff067ca48e2afdaff
luisbruhmuller <luis_bruhmuller@estudante.sesisenai.org.br>
2026-08-18 11:11:17 -0300

    fix - ponto e virgula maldito

--- public/cadastrar_prato.php
+++ public/cadastrar_prato.php
@@ -7,7 +7,7 @@ $nome = $_POST["nome"];
 $descricao = $_POST["descricao"];
 $preco = $_POST["preco"];
 $categoria = $_POST["categoria"];
-$id_usuario = $_POST["id_usuario"]
+$id_usuario = $_POST["id_usuario"];
 
 $sql = "INSERT INTO pratos (id,nome,descricao,preco,categoria,id_usuario) VALUES ('$id','$nome','$descricao','$preco','$categoria','$id_usuario')";
 
