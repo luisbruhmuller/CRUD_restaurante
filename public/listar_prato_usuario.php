1e42906ea1f2ce970509236ad092924445d6ffde
arthurdmachado <arthur_d_machado@estudante.sesisenai.org.br>
2026-08-19 12:33:12 -0300

    add - prepared estatements

--- public/listar_prato_usuario.php
+++ public/listar_prato_usuario.php
@@ -2,16 +2,22 @@
 
 include "../infra/conexao.php";
 
-$id_usuario = $_GET["id_usuario"];
-$sql = "SELECT * FROM pratos WHERE id_usuario = $id_usuario";
-$resultado = mysqli_query($conexao, $sql );
+$id_usuario = $_GET["id_usuario"] ?? null;
 
-$pratos = mysqli_query($conexao, "SELECT * FROM pratos");
+if (!$id_usuario || !is_numeric($id_usuario)) {
+    die("ID de usuário inválido.");
+}
+
+$sql = "SELECT * FROM pratos WHERE id_usuario = ?";
+$stmt = mysqli_prepare($conexao, $sql);
+mysqli_stmt_bind_param($stmt, "i", $id_usuario);
+mysqli_stmt_execute($stmt);
+$pratos = mysqli_stmt_get_result($stmt);
 
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
@@ -25,37 +31,34 @@ $pratos = mysqli_query($conexao, "SELECT * FROM pratos");
         <h1>CRUD - Restaurante</h1>
     </header>
     <main>
-        <h2>listar por usuario</h2>
-            <table>
+        <h2>Listar por usuário</h2>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Descrição</th>
+                <th>Preço</th>
+                <th>Categoria</th>
+                <th>Ações</th>
+            </tr>
+            <?php while ($prato = mysqli_fetch_assoc($pratos)) { ?>
                 <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                    <th>Descrição</th>
-                    <th>Preço</th>
-                    <th>Categoria</th>
-                    <th>Ações</th>
+                    <td><?php echo htmlspecialchars($prato["id"]) ?></td>
+                    <td><?php echo htmlspecialchars($prato["nome"]) ?></td>
+                    <td><?php echo htmlspecialchars($prato["descricao"]) ?></td>
+                    <td><?php echo htmlspecialchars($prato["preco"]) ?></td>
+                    <td><?php echo htmlspecialchars($prato["categoria"]) ?></td>
+                    <td>
+                        <a href="public/editar_prato.php?id=<?php echo urlencode($prato["id"]) ?>">Editar</a>
+                        <a href="public/excluir_prato.php?id=<?php echo urlencode($prato["id"]) ?>">Excluir</a>
+                    </td>
                 </tr>
-                <?php while ($prato = mysqli_fetch_assoc($pratos)) { ?>
-                    <tr>
-                        <td><?php echo $prato["id"] ?></td>
-                        <td><?php echo $prato["nome"] ?></td>
-                        <td><?php echo $prato["descricao"] ?></td>
-                        <td><?php echo $prato["preco"] ?></td>
-                        <td><?php echo $prato["categoria"] ?></td>
-                        <td>
-                            <a href="public/editar_prato.php?id=<?php echo $prato["id"] ?>">Editar</a>
-                            <a href="public/excluir_prato.php?id=<?php echo $prato["id"] ?>">Excluir</a>
-                        </td>
-                    </tr>
-                <?php } ?>
-            </table>
-        </div>
-
+            <?php } ?>
+        </table>
     </main>
     <footer>
 
     </footer>
-
 
 </body>
 
